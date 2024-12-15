f97cf8beb96f207c87e0bd7112bdfe68df13d464
Flor-Gust <luan.flor05@gmail.com>
2024-12-15 17:13:43 -0300

    Add models and repositories for Pessoa, Servicos, and Atendimento

---
+++ src/main/java/com/web/barbearia/model/enums/TipoPessoa.java
@@ -0,0 +1,24 @@
+package com.web.barbearia.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum TipoPessoa {
+    USER(1),
+    ADM(2);
+
+    private int value;
+
+    private TipoPessoa(int value) {
+        this.value = value;
+    }
+
+    public static TipoPessoa fromValue(int value) {
+        for (TipoPessoa tipo : TipoPessoa.values()) {
+            if (tipo.getValue() == value) {
+                return tipo;
+            }
+        }
+        throw new IllegalArgumentException("Valor inválido: " + value);
+    }
+}
